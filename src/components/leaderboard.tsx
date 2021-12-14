39894508990ee7b790762991934a5a0e8540dba9
rivkareich <rivka.reich@checkmarx.com>
2021-12-14 20:26:11 +0200

    leaderboard

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -1,7 +1,9 @@
 import "./login.less";
 import background from "../images/backleader.png";
-import logo from "../images/logo.png";
-import InputBase from "@material-ui/core/InputBase";
+import gamer1 from "../images/gam1.png";
+import gamer2 from "../images/gamer2.png";
+import gamer3 from "../images/gamer3.png";
+import linkedin from "../images/linkedin.png";
 
 const Login = () => (
   <div
@@ -13,7 +15,79 @@ const Login = () => (
       backgroundSize: "cover",
     }}
   >
-    <div>Best 2800</div>
+    <div
+      style={{
+        border: "3px solid #FF42A1",
+        borderRadius: "9rem",
+        width: "7rem",
+        height: "7rem",
+        position: "absolute",
+        marginLeft: "1.8rem",
+        marginTop: "4rem",
+      }}
+    >
+      <img
+        style={{
+          width: "6rem",
+          marginLeft: "0.5rem",
+        }}
+        src={gamer1}
+      />
+    </div>
+    <div
+      style={{
+        border: "3px solid #FF42A1",
+        borderRadius: "3rem",
+        width: "6rem",
+        height: "6rem",
+        position: "absolute",
+        marginLeft: "9rem",
+        marginTop: "4rem",
+        fontWeight: "700",
+      }}
+    >
+      <div style={{ marginLeft: "2rem", marginTop: "1.5rem" }}>Best</div>
+      <div style={{ marginLeft: "1.8rem", marginTop: "0.3rem" }}>2800</div>
+    </div>
+    <div
+      style={{
+        border: "3px solid #FF42A1",
+        borderRadius: "6rem",
+        width: "8rem",
+        height: "8rem",
+        position: "absolute",
+        marginLeft: "13.4rem",
+        marginTop: "8rem",
+        fontWeight: "700",
+      }}
+    >
+      <div style={{ marginLeft: "2.8rem", marginTop: "2.5rem" }}>Score</div>
+      <div style={{ marginLeft: "2.8rem", marginTop: "0.3rem" }}>2800</div>
+    </div>
+    <div
+      style={{
+        border: "3px solid #FF42A1",
+        borderRadius: "5rem",
+        width: "7rem",
+        height: "7rem",
+        position: "absolute",
+        marginLeft: "6.2rem",
+        marginTop: "9.8rem",
+        fontWeight: "700",
+      }}
+    >
+      <div style={{ marginLeft: "2.6rem", marginTop: "2rem" }}>Rate</div>
+      <div style={{ marginLeft: "2.9rem", marginTop: "0.3rem" }}>2/3</div>
+    </div>
+    <img
+      style={{
+        width: "4rem",
+        position: "absolute",
+        marginLeft: "16.5rem",
+        marginTop: "5.5rem",
+      }}
+      src={linkedin}
+    />
     <div
       style={{
         display: "flex",
@@ -35,7 +109,15 @@ const Login = () => (
           backgroundColor: "#F886B6",
         }}
       >
-        <div style={{position: "absolute", marginLeft: "-1.5rem", marginTop: "0.6rem"}}>1</div>
+        <div
+          style={{
+            position: "absolute",
+            marginLeft: "-1.5rem",
+            marginTop: "0.6rem",
+          }}
+        >
+          1
+        </div>
         <div
           onClick={() => (window.location.href = "/home")}
           style={{
@@ -44,8 +126,18 @@ const Login = () => (
             cursor: "pointer",
           }}
         >
-         <div style={{ justifyContent: "space-around", display: "flex" }}>
-            <span>Sara</span>
+          <div style={{ justifyContent: "space-around", display: "flex" }}>
+            <div style={{ display: "flex" }}>
+              <img
+                style={{
+                  width: "2rem",
+                  marginTop: "-0.4rem",
+                  marginLeft: "-1rem",
+                }}
+                src={gamer2}
+              />
+              <span>Sara</span>
+            </div>
             <span>2000 points</span>
           </div>
         </div>
@@ -61,7 +153,15 @@ const Login = () => (
           backgroundColor: "#F886B6",
         }}
       >
-        <div style={{position: "absolute", marginLeft: "-1.5rem", marginTop: "0.6rem"}}>2</div>
+        <div
+          style={{
+            position: "absolute",
+            marginLeft: "-1.5rem",
+            marginTop: "0.6rem",
+          }}
+        >
+          2
+        </div>
         <div
           onClick={() => (window.location.href = "/home")}
           style={{
@@ -70,8 +170,18 @@ const Login = () => (
             cursor: "pointer",
           }}
         >
-         <div style={{ justifyContent: "space-around", display: "flex" }}>
-            <span>You</span>
+          <div style={{ justifyContent: "space-around", display: "flex" }}>
+            <div style={{ display: "flex" }}>
+              <img
+                style={{
+                  width: "2rem",
+                  marginTop: "-0.4rem",
+                  marginLeft: "-1rem",
+                }}
+                src={gamer1}
+              />
+              <span>You</span>
+            </div>
             <span>1500 points</span>
           </div>
         </div>
@@ -87,7 +197,15 @@ const Login = () => (
           backgroundColor: "#F886B6",
         }}
       >
-        <div style={{position: "absolute", marginLeft: "-1.5rem", marginTop: "0.6rem"}}>3</div>
+        <div
+          style={{
+            position: "absolute",
+            marginLeft: "-1.5rem",
+            marginTop: "0.6rem",
+          }}
+        >
+          3
+        </div>
         <div
           onClick={() => (window.location.href = "/home")}
           style={{
@@ -97,7 +215,17 @@ const Login = () => (
           }}
         >
           <div style={{ justifyContent: "space-around", display: "flex" }}>
-            <span>Dan</span>
+            <div style={{ display: "flex" }}>
+              <img
+                style={{
+                  width: "2rem",
+                  marginTop: "-0.4rem",
+                  marginLeft: "-1rem",
+                }}
+                src={gamer3}
+              />
+              <span>Dan</span>
+            </div>
             <span>1200 points</span>
           </div>
         </div>

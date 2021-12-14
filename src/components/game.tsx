785bed86102ca3cf22ec803a95517195ac223ab1
rivkareich <rivka.reich@checkmarx.com>
2021-12-14 10:32:16 +0200

    add iframe

---
+++ src/components/game.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ display: "flex" }}>
+      <iframe
+        style={{ width: "100vw", height: "99.6vh" }}
+        src="https://codebashing-dev-lms.s3.eu-west-1.amazonaws.com/lms/hackaton-cb-master/CardsAgainstVulnerabilities_KU2_Web/story.html"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
